3416f1f68a59bd1707512fca7b38aa66fd45f661
rikimario <mario_martinov@abv.bg>
2024-06-04 16:31:51 +0300

    install tailwind chadcn and fix navigation for mobile and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navigation from "./components/navigation/Navigation";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navigation />
     </>
   );
 }
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,55 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  const content = (
+    <>
+      <div className="absolute left-0 right-0 block w-full transition md:hidden top-16 bg bg-[#1c2226]">
+        <ul className="p-20 text-xl text-center text-[#fff]">
+          <li spy={true} className="py-4 my-4 border-b border-slate-700">
+            <a href="#home">Home</a>
+          </li>
+          <li className="py-4 my-4 border-b border-slate-700">
+            <a href="#about">About</a>
+          </li>
+          <li className="py-4 my-4 border-b border-slate-700">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="py-4 my-4 border-b border-slate-700">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+  return (
+    <nav>
+      <div className="flex items-center justify-between text-[#fff] pt-5 px-10 md:px-16 lg:px-24 xl:px-44 md:pt-10">
+        <div className="flex-1 text-xl font-semibold md:text-3xl">M V</div>
+        <div className="lg:flex md:flex items-center justify-between text-[#fff] hidden">
+          <ul className="flex lg:gap-20 lg:text-lg md:gap-10">
+            <li className="hover:text-[#0080FF]">
+              <a href="#home">Home</a>
+            </li>
+            <li className="hover:text-[#0080FF]">
+              <a href="#about">About</a>
+            </li>
+            <li className="hover:text-[#0080FF]">
+              <a href="#projects">Projects</a>
+            </li>
+            <li className="hover:text-[#0080FF]">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>{isOpen && content}</div>
+
+        <button className="block transition sm:hidden" onClick={handleClick}>
+          {isOpen ? <X /> : <Menu />}
+        </button>
+      </div>
+    </nav>
+  );
+}
