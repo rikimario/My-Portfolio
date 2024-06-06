c3c50905d0c6f0c457c5cffcaadd12015f6c3d1e
rikimario <mario_martinov@abv.bg>
2024-06-06 18:07:16 +0300

    add scroll to nav ul and handle close to mobile nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-scroll";
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,19 +10,24 @@ export default function Navigation() {
       <div className="absolute z-20 h-screen left-0 right-0 block w-full transition md:hidden top-16 bg bg-[#1c2226]">
         <ul className="p-20 text-xl text-center text-[#fff]">
           <li className="py-4 my-4 border-b border-slate-700">
-            <a href="#home">Home</a>
+            <Link onClick={handleClick} spy={true} smooth={true} to="about">
+              About
+            </Link>
           </li>
           <li className="py-4 my-4 border-b border-slate-700">
-            <a href="#about">About</a>
+            <Link onClick={handleClick} spy={true} smooth={true} to="skills">
+              Skills
+            </Link>
           </li>
           <li className="py-4 my-4 border-b border-slate-700">
-            <a href="#skills">Skills</a>
+            <Link onClick={handleClick} spy={true} smooth={true} to="projects">
+              Projects
+            </Link>
           </li>
           <li className="py-4 my-4 border-b border-slate-700">
-            <a href="#projects">Projects</a>
-          </li>
-          <li className="py-4 my-4 border-b border-slate-700">
-            <a href="#contact">Contact</a>
+            <Link onClick={handleClick} spy={true} smooth={true} to="contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
@@ -30,23 +36,35 @@ export default function Navigation() {
   return (
     <nav>
       <div className="flex items-center justify-between text-[#fff] pt-5 px-10 md:px-16 lg:px-24 xl:px-44 md:pt-10">
-        <div className="flex-1 text-xl font-semibold md:text-3xl">M V</div>
+        {/* <div className="flex-1 text-xl font-semibold md:text-3xl">
+          <span className="text-[#0080FF]">{"< "}</span>M V{" "}
+          <span className="text-[#0080FF]">{" />"}</span>
+        </div> */}
+        <div className="flex-1 text-xl font-semibold md:text-3xl">
+          <span>{"< "}</span> <span className="text-[#0080FF]">M V</span>{" "}
+          <span>{" />"}</span>
+        </div>
         <div className="lg:flex md:flex items-center justify-between text-[#fff] hidden">
           <ul className="flex lg:gap-20 lg:text-lg md:gap-10">
-            <li className="hover:text-[#0080FF]">
-              <a href="#home">Home</a>
+            <li className="hover:text-[#0080FF] cursor-pointer">
+              <Link spy={true} smooth={true} to="about">
+                About
+              </Link>
             </li>
-            <li className="hover:text-[#0080FF]">
-              <a href="#about">About</a>
+            <li className="hover:text-[#0080FF] cursor-pointer">
+              <Link spy={true} smooth={true} to="skills">
+                Skills
+              </Link>
             </li>
-            <li className="hover:text-[#0080FF]">
-              <a href="#skills">Skills</a>
+            <li className="hover:text-[#0080FF] cursor-pointer">
+              <Link spy={true} smooth={true} to="projects">
+                Projects
+              </Link>
             </li>
-            <li className="hover:text-[#0080FF]">
-              <a href="#projects">Projects</a>
-            </li>
-            <li className="hover:text-[#0080FF]">
-              <a href="#contact">Contact</a>
+            <li className="hover:text-[#0080FF] cursor-pointer">
+              <Link spy={true} smooth={true} to="contact">
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
